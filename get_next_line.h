7c2e341f704253c592c57b794b8b67fb28e44a5c
Bohdan <81105398+bosass@users.noreply.github.com>
2025-01-21 20:20:05 +0100

    Beheading

--- get_next_line.h
+++ get_next_line.h
@@ -10,4 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-get next head;
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 100
+#endif
+
+char *get_next_line(int fd);
+
+char *reading(int fd, char *stringread);
+char *alocating(char *stringout);
+char *stringwriting(char *stringread, char *stringout, int i);
+
+#endif
